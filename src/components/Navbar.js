7d4f5698febc436e1a030b8063daa21a124b238d
leen-aj <22430566@students.liu.edu.lb>
2026-05-30 23:11:01 +0300

    Fix responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaCrown, FaHeart, FaShoppingBag, FaUser } from "react-icons/fa";
 
-
 function Navbar() {
   return (
     <>
@@ -9,66 +8,73 @@ function Navbar() {
         FREE SHIPPING ON ALL ORDERS
       </div>
 
-<nav className="navbar navbar-expand-lg sticky-top">
-  <div className="container-fluid px-4">
+      <nav className="navbar navbar-expand-lg sticky-top bg-black">
+        <div className="container-fluid px-4">
 
-    <Link className="navbar-brand fw-bold" to="/">
-      <FaCrown className="me-2" />
-      Amour
-    </Link>
-
- 
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarContent"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-
-
-    <div
-      className="collapse navbar-collapse"
-      id="navbarContent"
-    >
-
-      <ul className="navbar-nav ms-auto">
-
-        <li className="nav-item">
-          <Link to="/product" className="nav-link">
-            Shop
+          <Link className="navbar-brand fw-bold" to="/">
+            <FaCrown className="me-2" />
+            Amour
           </Link>
-        </li>
 
-      </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-      <Link
-        to="/measurement"
-        className="btn btn-pink ms-lg-3"
-      >
-        Measurements
-      </Link>
+          <div
+            className="collapse navbar-collapse"
+            id="navbarContent"
+          >
+            <ul className="navbar-nav ms-auto">
 
-      <div className="ms-lg-3 icons d-flex gap-3">
+              <li className="nav-item">
+                <Link to="/product" className="nav-link">
+                  Shop
+                </Link>
+              </li>
 
-        <Link to="/wishlist">
-          <FaHeart />
-        </Link>
+            </ul>
 
-        <Link to="/cart">
-          <FaShoppingBag />
-        </Link>
+            <Link
+              to="/measurement"
+              className="btn btn-pink ms-lg-3 my-2 my-lg-0"
+            >
+              Measurements
+            </Link>
 
-        <Link to="/account">
-          <FaUser />
-        </Link>
+            <div className="icons ms-lg-3">
 
-      </div>
+              <Link
+                to="/wishlist"
+                className="text-decoration-none me-3"
+              >
+                <FaHeart color="#ffb6d9" />
+              </Link>
 
-    </div>
-  </div>
-</nav>
+              <Link
+                to="/cart"
+                className="text-decoration-none me-3"
+              >
+                <FaShoppingBag color="#ffb6d9" />
+              </Link>
+
+              <Link
+                to="/account"
+                className="text-decoration-none"
+              >
+                <FaUser color="#ffb6d9" />
+              </Link>
+
+            </div>
+
+          </div>
+
+        </div>
+      </nav>
     </>
   );
 }
